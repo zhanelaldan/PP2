9e2f477f93c6ea7c80bf8e51f231534f2a934103
zhanelaldan <aldan.zhanel@mail.ru>
2023-02-26 23:36:00 +0600

    finished lab4

--- lab4/json/1.py
+++ lab4/json/1.py
@@ -1,14 +1,16 @@
 import json
-with open("data.json", "r") as read_file:
+with open("sample-data.json", "r") as read_file:
     data = json.load(read_file)
-print(data)
 print("""Interface Status
 ================================================================================""")
-print("""DN                                             Description          Speed                      MTU """)
-for i, k in data["imdata"][0]['l1PhysIf']["attributes"].items():
-    if i == 'dn':
-        print(k, end="                          ")
-    if i == "speed":
-        print(k, end="                                         ")
-    if i == "mtu":
-        print(k, end="                   ")
+print("""DN                                                  Description          Speed     MTU""") 
+print("""--------------------------------------------------  -------------------- ------   ------""")
+for s in range(18):
+    for i, k in data["imdata"][s]['l1PhysIf']["attributes"].items():
+        if i == 'dn':
+            print("{:<73}".format(k),end="")
+        if i == "fecMode":
+            print("{:<10}".format(k),end="")
+        if i == "mtu":
+            print("{:<1}".format(k),end="")
+    print("\n")
